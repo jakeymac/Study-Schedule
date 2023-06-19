6f25abfc98b1bdf6fe049b3203920461d07b5f51
Jacob Johnson <jakeymacaroni@gmail.com>
2023-06-19 01:01:59 -0600

    main.py comments
    Added comments to main.py

--- main.py
+++ main.py
@@ -3,9 +3,10 @@ from window import *
 
 import sqlite3 as sql
 
+#Initiate server connection and cursor for queries
 connect = sql.connect("server.db")
 cursor = connect.cursor()
 
-db = DatabaseAccess(cursor,connect)
-window = Window(db)
-print("hi")
+
+db = DatabaseAccess(cursor,connect) #Create database access object to pass to the window
+window = Window(db) #Create window object
