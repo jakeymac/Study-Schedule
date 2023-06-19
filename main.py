9962cde6b4842af066891666c27cf1b88bf16aed
Jacob Johnson <jakeymacaroni@gmail.com>
2023-06-19 01:00:58 -0600

    Reorganized into window and DatabaseAccess.py
    Reorganized everything into seperate files, now main.py gets everything running

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
+from DatabaseAccess import *
+from window import *
+
 import sqlite3 as sql
 
 connect = sql.connect("server.db")
 cursor = connect.cursor()
+
+db = DatabaseAccess(cursor,connect)
+window = Window(db)
+print("hi")
